d455fc5a3e9a8390887e053824c6db7da564fe68
Evan Mattson <me@aaemnnost.tv>
2016-08-29 17:11:09 +0400

    alternate install for subsplit

--- deploy/deploy-plugin.sh
+++ deploy/deploy-plugin.sh
@@ -6,8 +6,8 @@ ssh-add deploy/key
 # Install git-subsplit
 (
     cd /tmp
-    git clone https://github.com/dflydev/git-subsplit.git
-    bash git-subsplit/install.sh
+    wget https://github.com/dflydev/git-subsplit/raw/d77ec9d3e1addd97dca1464eabf95c525f591490/git-subsplit.sh
+    cp git-subsplit.sh "$(git --exec-path)"/git-subsplit
 )
 # Prepare for doing the subsplits
 git subsplit init git@github.com:aaemnnosttv/wp-cli-login-command.git
