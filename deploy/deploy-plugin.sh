3bf36ab1b85c745abe85967bb5dd1588a4dfc768
Evan Mattson <me@aaemnnost.tv>
2016-08-29 16:46:35 +0400

    don’t forget to install git-subsplit, add comments

--- deploy/deploy-plugin.sh
+++ deploy/deploy-plugin.sh
@@ -1,7 +1,15 @@
-eval "$(ssh-agent -s)" #start the ssh agent
+# Provide SSH access to the repository
+eval "$(ssh-agent -s)"
 chmod 600 deploy/key
 ssh-add deploy/key
 
+# Install git-subsplit
+(
+    cd /tmp
+    git clone git@github.com:dflydev/git-subsplit.git
+    bash git-subsplit/install.sh
+)
+# Prepare for doing the subsplits
 git subsplit init git@github.com:aaemnnosttv/wp-cli-login-command.git
-
+# synchronize the plugin directory with its respective repository for the current branch
 git subsplit publish --heads="$TRAVIS_BRANCH" plugin/:git@github.com:aaemnnosttv/wp-cli-login-server.git
