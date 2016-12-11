1c2453b84ba14e4afe64cf8370867ec79525f1a3
Evan Mattson <me@aaemnnost.tv>
2016-12-11 23:12:14 +0200

    add unit test for uri parsing

---
+++ tests/unit/WP_CLI_Login_ServerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use WP_CLI_Login\WP_CLI_Login_Server;
+
+class WP_CLI_Login_ServerTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    function parses_endpoint_and_key_from_uri()
+    {
+        list($endpoint, $public) = WP_CLI_Login_Server::parseUri('/end/key');
+
+        $this->assertSame('end', $endpoint);
+        $this->assertSame('key', $public);
+    }
+
+    /** @test */
+    function parses_endpoint_and_key_from_uri_for_subdirectory_site()
+    {
+        list($endpoint, $public) = WP_CLI_Login_Server::parseUri('/abc/end/key');
+
+        $this->assertSame('end', $endpoint);
+        $this->assertSame('key', $public);
+    }
+
+}
