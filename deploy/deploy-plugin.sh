4ecb9259f962efa55f15d04560187bf56ef851cd
Evan Mattson <me@aaemnnost.tv>
2017-04-16 13:31:59 +0300

    update deploy script to tag server plugin by tag

--- deploy/deploy-plugin.sh
+++ deploy/deploy-plugin.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Provide SSH access to the repository
 eval "$(ssh-agent -s)"
 chmod 600 deploy/key
@@ -8,4 +9,9 @@ wget https://cdn.rawgit.com/dflydev/git-subsplit/d77ec9d3e1addd97dca1464eabf95c5
 # Prepare for doing the subsplits
 bash git-subsplit.sh init git@github.com:aaemnnosttv/wp-cli-login-command.git
 # synchronize the plugin directory with its respective repository for the current branch
-bash git-subsplit.sh publish --tags="$TRAVIS_TAG" plugin/:git@github.com:aaemnnosttv/wp-cli-login-server.git
+bash git-subsplit.sh publish --heads=master --no-tags plugin/:git@github.com:aaemnnosttv/wp-cli-login-server.git
+
+# The server plugin will be tagged 'server-x.x.x'.
+# Strip the prefix and tag the server repo with 'x.x.x'
+git tag "${TRAVIS_TAG#server-}" "$TRAVIS_COMMIT" --path="${PWD}/.subsplit"
+git push origin master --tags --path="${PWD}/.subsplit"
