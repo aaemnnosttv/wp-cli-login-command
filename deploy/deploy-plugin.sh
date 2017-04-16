700e1f345eb901e4754b485496fd04f07f298736
Evan Mattson <me@aaemnnost.tv>
2017-04-16 13:28:02 +0300

    use rawgit cdn for git subsplit

--- deploy/deploy-plugin.sh
+++ deploy/deploy-plugin.sh
@@ -4,7 +4,7 @@ chmod 600 deploy/key
 ssh-add deploy/key
 
 # Download git-subsplit
-wget https://github.com/dflydev/git-subsplit/raw/d77ec9d3e1addd97dca1464eabf95c525f591490/git-subsplit.sh
+wget https://cdn.rawgit.com/dflydev/git-subsplit/d77ec9d3e1addd97dca1464eabf95c525f591490/git-subsplit.sh
 # Prepare for doing the subsplits
 bash git-subsplit.sh init git@github.com:aaemnnosttv/wp-cli-login-command.git
 # synchronize the plugin directory with its respective repository for the current branch
