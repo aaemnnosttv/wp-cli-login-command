ecccbbe2a8c076139074033d16864304410f89aa
Evan Mattson <me@aaemnnost.tv>
2017-04-30 00:06:12 +0300

    deploy to server repo

--- deploy/deploy-plugin.sh
+++ deploy/deploy-plugin.sh
@@ -4,15 +4,17 @@ eval "$(ssh-agent -s)"
 chmod 600 deploy/key
 ssh-add deploy/key
 
+PLUGIN_REPO=git@github.com:aaemnnosttv/wp-cli-login-server.git
+
 # Download git-subsplit
 wget https://cdn.rawgit.com/dflydev/git-subsplit/d77ec9d3e1addd97dca1464eabf95c525f591490/git-subsplit.sh
 # Prepare for doing the subsplits
 bash git-subsplit.sh init git@github.com:aaemnnosttv/wp-cli-login-command.git
 # synchronize the plugin directory with its respective repository for the current branch
-bash git-subsplit.sh publish --heads=master --no-tags plugin/:git@github.com:aaemnnosttv/wp-cli-login-server.git
+bash git-subsplit.sh publish --heads=master --no-tags plugin/:${PLUGIN_REPO}
 
 # The server plugin will be tagged 'server-x.x.x'.
 # Strip the prefix and tag the server repo with 'x.x.x'
 cd .subsplit
-git tag "${TRAVIS_TAG#server-}" "$TRAVIS_COMMIT"
-git push origin master --tags
+git tag ${TRAVIS_TAG#server-}
+git push ${PLUGIN_REPO} --tags
