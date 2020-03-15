0565cab9ddb510f24e2367bc2b4d86fbf139b002
Evan Mattson <me@aaemnnost.tv>
2020-03-15 23:57:54 +0200

    Load local autoloader, if present

--- command.php
+++ command.php
@@ -6,6 +6,10 @@ if (! class_exists('WP_CLI')) {
     return;
 }
 
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
 LoginCommand::setPackage(new Package(__DIR__));
 
 \WP_CLI::add_command('login', LoginCommand::class);
